431b274fb82210b46d5b8b1ef48357e658728c26
Adam Hajduk <adam.p.hajduk@gmail.com>
2018-04-18 12:07:12 +0200

    Finishing TLS

--- artxchg/createArt.sh
+++ artxchg/createArt.sh
@@ -1,9 +1,17 @@
-docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-001\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-002\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-003\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-004\", \"foo\", \"bar\"]}'"
+docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-001\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-002\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-003\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org1 bash -c "peer chaincode invoke -C org1 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO1-004\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
 
-docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-001\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-002\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-003\", \"foo\", \"bar\"]}'"
-docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-004\", \"foo\", \"bar\"]}'"
+docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-001\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-002\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-003\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli0.Org2 bash -c "peer chaincode invoke -C org2 -n artxchg -c '{\"Args\":[\"createArt\", \"PaintO2-004\", \"foo\", \"bar\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
--- artxchg/queryArt.sh
+++ artxchg/queryArt.sh
@@ -1,2 +1,4 @@
-docker exec cli.ArtMarket bash -c "peer chaincode query -C org1 -n artxchg -c '{\"Args\":[\"queryStock\"]}'"
-docker exec cli.ArtMarket bash -c "peer chaincode query -C org2 -n artxchg -c '{\"Args\":[\"queryStock\"]}'"
+docker exec cli.ArtMarket bash -c "peer chaincode query -C org1 -n artxchg -c '{\"Args\":[\"queryStock\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
+docker exec cli.ArtMarket bash -c "peer chaincode query -C org2 -n artxchg -c '{\"Args\":[\"queryStock\"]}' \
+	--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/artxchg.com/orderers/orderer.artxchg.com/msp/tlscacerts/tlsca.artxchg.com-cert.pem"
