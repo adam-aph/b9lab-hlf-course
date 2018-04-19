72e24758289cd8a57396a7af2e76016ca99d8c30
Adam Hajduk <adam.p.hajduk@gmail.com>
2018-04-19 19:17:28 +0200

    fixed orderer port mapping

--- artxchg2/network.sh
+++ artxchg2/network.sh
@@ -1,5 +1,5 @@
 docker exec cli.ArtMarket bash -c 'peer channel create -c org1 -f ./channels/Org1.tx -o orderer0.artxchg2.com:7050 --timeout 30'
-docker exec cli.ArtMarket bash -c 'peer channel create -c org2 -f ./channels/Org2.tx -o orderer1.artxchg2.com:7050 --timeout 30'
+docker exec cli.ArtMarket bash -c 'peer channel create -c org2 -f ./channels/Org2.tx -o orderer0.artxchg2.com:7050 --timeout 30'
 
 docker exec cli.ArtMarket bash -c 'peer channel join -b org1.block'
 docker exec cli.ArtMarket bash -c 'peer channel join -b org2.block'
@@ -11,8 +11,8 @@ docker exec cli1.Org2 bash -c 'peer channel join -b org2.block'
 docker exec cli0.Org3 bash -c 'peer channel join -b org2.block'
 
 docker exec cli0.Org1 bash -c 'peer channel update -o orderer0.artxchg2.com:7050 -c org1 -f ./channels/org1anchor.tx'
-docker exec cli0.Org2 bash -c 'peer channel update -o orderer1.artxchg2.com:7050 -c org2 -f ./channels/org2anchor.tx'
-docker exec cli.ArtMarket bash -c 'peer channel update -o orderer2.artxchg2.com:7050 -c org1 -f ./channels/marketanchororg1.tx'
+docker exec cli0.Org2 bash -c 'peer channel update -o orderer0.artxchg2.com:7050 -c org2 -f ./channels/org2anchor.tx'
+docker exec cli.ArtMarket bash -c 'peer channel update -o orderer0.artxchg2.com:7050 -c org1 -f ./channels/marketanchororg1.tx'
 docker exec cli.ArtMarket bash -c 'peer channel update -o orderer0.artxchg2.com:7050 -c org2 -f ./channels/marketanchororg2.tx'
 
 docker exec cli0.Org1 bash -c 'peer chaincode install -p artxchg2 -n artxchg2 -v 0'
